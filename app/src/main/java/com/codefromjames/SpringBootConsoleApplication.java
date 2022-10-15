2b422be8a47b7ccfd1d3118a1e06cd45e8b40808
James O'Cull <james.ocull@gmail.com>
2022-10-15 12:29:00 -0400

    Debugging backups, failovers, adding notes

--- app/src/main/java/com/codefromjames/SpringBootConsoleApplication.java
+++ app/src/main/java/com/codefromjames/SpringBootConsoleApplication.java
@@ -1,7 +1,9 @@
 package com.codefromjames;
 
+import org.apache.commons.lang3.mutable.MutableInt;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.client.ClientCache;
+import org.apache.ignite.client.ClientCacheConfiguration;
 import org.apache.ignite.client.ClientException;
 import org.apache.ignite.client.IgniteClient;
 import org.apache.ignite.configuration.ClientConfiguration;
@@ -11,6 +13,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.Instant;
 import java.util.Set;
 import java.util.UUID;
 
@@ -25,7 +28,7 @@ public class SpringBootConsoleApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) {
+    public void run(String... args) throws InterruptedException {
         LOGGER.info("EXECUTING : command line runner");
 
         for (int i = 0; i < args.length; ++i) {
@@ -34,16 +37,30 @@ public class SpringBootConsoleApplication implements CommandLineRunner {
 
         final ClientConfiguration cfg = new ClientConfiguration()
                 .setAddresses("localhost:10800", "localhost:10801", "localhost:10802")
-                .setPartitionAwarenessEnabled(true);
+                .setPartitionAwarenessEnabled(true)
+                .setReconnectThrottlingPeriod(5_000);
 
         try (final IgniteClient client = Ignition.startClient(cfg)) {
-            final ClientCache<Integer, String> cache = client.getOrCreateCache("myCache");
-            LOGGER.info("Previous: {}", cache.getAll(Set.of(1, 2)).values());
-            cache.put(1, "hello " + UUID.randomUUID());
-            cache.put(2, "world " + UUID.randomUUID());
-            LOGGER.info("New: {}", cache.getAll(Set.of(1, 2)).values());
-        } catch (ClientException ex) {
-            LOGGER.error("DB failed", ex);
+            final ClientCache<Integer, String> cache = client.cache("myCache"); // Should already be created per config!
+            final MutableInt counter = new MutableInt(0);
+            while (true) {
+                try {
+                    int a = counter.getAndIncrement();
+                    int b = counter.getAndIncrement();
+                    if (counter.getValue() > 100) {
+                        counter.setValue(0);
+                    }
+
+                    final Instant now = Instant.now();
+                    LOGGER.info("Previous {}, {}: {}", a, b, cache.getAll(Set.of(a, b)).values());
+                    cache.put(a, "hello " + now);
+                    cache.put(b, "world " + now);
+                    LOGGER.info("New {}, {}: {}", a, b, cache.getAll(Set.of(a, b)).values());
+                } catch (ClientException ex) {
+                    LOGGER.error("DB failed", ex);
+                }
+                Thread.sleep(250);
+            }
         }
     }
 }
---
+++ notes.md
@@ -0,0 +1,19 @@
+If a partition loses data because owners all died you must acknowledge loss by resetting the cache state:
+
+```bash
+./control.sh --host ignite01 --cache reset_lost_partitions myCache
+# Or...
+docker exec -it apache-ignite-cluster-ignite01-1 /opt/ignite/apache-ignite/bin/control.sh --host ignite01 --cache reset_lost_partitions myCache
+```
+
+If you have a cache group, you need to reset by the group:
+
+```bash
+docker exec -it apache-ignite-cluster-ignite01-1 /opt/ignite/apache-ignite/bin/control.sh --host ignite01 --cache reset_lost_partitions debugGroup
+```
+
+Partition loss matters:
+- `READ_ONLY_SAFE` no one can write anything to any partition (this is a pedantic level of safety)
+- `READ_WRITE_SAFE` you can write but only to available partitions (this option seems most appropriate if data is well-partitioned)
+- `IGNORE` would allow writes regardless, but can only be used with in-memory mode (no disk persistence)
+
