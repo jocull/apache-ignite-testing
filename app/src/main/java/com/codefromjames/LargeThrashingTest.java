196bcf82f33e7ec38d0012b7e2ef6ea49b4a5f61
James O'Cull <james.ocull@gmail.com>
2022-10-16 23:11:50 -0400

    Improve load validating test

--- app/src/main/java/com/codefromjames/LargeThrashingTest.java
+++ app/src/main/java/com/codefromjames/LargeThrashingTest.java
@@ -9,32 +9,49 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Instant;
-import java.util.Optional;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LargeThrashingTest implements Runnable {
     private static final Logger LOGGER = LoggerFactory.getLogger(LargeThrashingTest.class);
     private static final Random RANDOM = new Random();
 
     private final IgniteClient client;
+    private final List<String> keySet;
+    private final Map<String, String> ackedKeyHashes = new HashMap<>();
 
     public LargeThrashingTest(IgniteClient client) {
         this.client = client;
+        this.keySet = IntStream.range(0, 1024)
+                .mapToObj(i -> UUID.randomUUID().toString())
+                .collect(Collectors.toList());
     }
 
     @Override
     public void run() {
         final ClientCache<String, byte[]> cache = client.cache("myCache"); // Should already be created per config!
         final MutableInt counter = new MutableInt(0);
+        final Deque<String> keyRotation = new ArrayDeque<>(keySet);
         while (true) {
+            if (keyRotation.isEmpty()) {
+                keyRotation.addAll(keySet);
+            }
+            final LargeObject largeObject = new LargeObject(keyRotation.pop());
             try {
-                final Instant now = Instant.now();
-                final LargeObject largeObject = new LargeObject();
-                Optional<String> existingHash = Optional.ofNullable(cache.get(largeObject.key))
-                                .map(DigestUtils::sha1Hex);
+                Optional.ofNullable(cache.get(largeObject.key))
+                                .map(DigestUtils::sha1Hex)
+                                        .ifPresent(currentHash -> {
+                                            final String ackedHash = ackedKeyHashes.get(largeObject.key);
+                                            if (ackedHash != null
+                                                    && !ackedHash.equals(currentHash)) {
+                                                LOGGER.warn("Current key {} hash {} does not match {}", largeObject.key, currentHash, ackedHash);
+                                            }
+                                        });
+
                 cache.put(largeObject.key, largeObject.blob);
                 LOGGER.info("Wrote #{}, {} w/ hash {}", counter.getAndIncrement(), largeObject.key, largeObject.hash);
+                ackedKeyHashes.put(largeObject.key, largeObject.hash);
             } catch (ClientException ex) {
                 LOGGER.error("DB failed", ex);
             }
@@ -52,8 +69,8 @@ public class LargeThrashingTest implements Runnable {
         final byte[] blob = new byte[1024 * 1024]; // 1 MB
         final String hash;
 
-        LargeObject() {
-            this.key = UUID.randomUUID().toString();
+        LargeObject(String key) {
+            this.key = key;
             RANDOM.nextBytes(blob);
             this.hash = DigestUtils.sha1Hex(blob);
         }
